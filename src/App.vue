006ca95fca0e8c4bf7a271dd12bc258eddd65bb1
Udeorah Frank <franklynudeorah@yahoo.com>
2023-06-25 09:17:17 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloRedack from './components/HelloRedack.vue';
+
+</script>
+
+<template>
+  <div>
+    <HelloRedack msg="Hello from Redack" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloRedack.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <h1 class=" text-lg">{{ msg }}</h1>
+</template>
+
+<style scoped>
+</style>
