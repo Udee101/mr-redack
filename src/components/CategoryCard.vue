e96a8e9f10f04a0287b699a3e262aaa59cf76c1a
Udeorah Frank <franklynudeorah@yahoo.com>
2023-07-13 22:19:02 +0100

    create skills section

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,19 +1,20 @@
 <script setup>
   defineProps({
-    imgSrc: String,
-    name: String,
-    categoryId: Number 
+    pc: Object,
+    // imgSrc: String,
+    // name: String,
+    // categoryId: Number 
   })
 </script>
 
 <template>
   <div class="grid grid-flow-col place-items-center lg:self-center">
     <router-link 
-      :to="{ name: 'categories.id', params: { id: categoryId } }"
+      :to="{ name: 'categories.id', params: { id: pc.id } }"
       class="category-card"
     >
-      <img :src="imgSrc" alt="category image" class="w-[60%] lg:w-[75%] mx-auto" />
-      <p>{{ name }}</p>
+      <img :src="pc.imgSrc" alt="category image" class="w-[60%] lg:w-[75%] mx-auto" />
+      <p>{{ pc.name }}</p>
     </router-link>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,13 @@ import AboutSection from '../components/sections/AboutSection.vue';
 import ContactMeSection from '../components/sections/ContactMeSection.vue';
 import HeroSection from '../components/sections/HeroSection.vue';
 import PortfolioSection from '../components/sections/PortfolioSection.vue';
+import SkillsSection from '../components/sections/SkillsSection.vue';
 </script>
 
 <template>
   <HeroSection></HeroSection>
   <PortfolioSection></PortfolioSection>
   <AboutSection></AboutSection>
+  <SkillsSection></SkillsSection>
   <ContactMeSection></ContactMeSection>
 </template>
