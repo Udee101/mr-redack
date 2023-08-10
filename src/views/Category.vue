5b73dbd791467fbeb89a5929bd3356277cb55af5
Udeorah Frank <franklynudeorah@gmail.com>
2023-08-10 05:06:03 +0100

    update:modify project data import in category page

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -26,9 +26,9 @@ onMounted(() => {
   <section class="p-4 bg-site-gray">
     <div v-if="isLoading" class="text-white font-bold h-screen w-full flex items-center mx-auto"><p>LOADING.....</p></div>
     <div v-else class=" relative max-w-site-w mx-auto min-h-screen py-14">
-      <p class=" hidden absolute right-0 mx-2 py-10 text-site-gold text-4xl font-titleFont md:block">{{ categoryData[id-1].tag }}</p>
-      <p class="text-center font-titleFont text-3xl text-site-gold pt-12  md:hidden">{{ categoryData[id-1].tag }}</p>
-      <div v-for="(data, index) in categoryData[id-1].titles" :key="index" class="min-h-[70vh] flex flex-col">
+      <p class=" hidden absolute right-0 mx-2 py-10 text-site-gold text-4xl font-titleFont md:block">{{ projectData[id-1].tag }}</p>
+      <p class="text-center font-titleFont text-3xl text-site-gold pt-12  md:hidden">{{ projectData[id-1].tag }}</p>
+      <div v-for="(data, index) in projectData[id-1].titles" :key="index" class="min-h-[70vh] flex flex-col">
         <div class="grid first:mt-6 md:first:mt-40 md:grid-cols-2 md:gap-x-6">
           <ProjectDetails
             :category-data="projectData[id-1]"
@@ -40,43 +40,3 @@ onMounted(() => {
     </div>
   </section>
 </template>
-
-<style>
-/* .splide{
-  padding: 3rem 1rem;
-}
-.splide__arrow {
-  display: none;
-}
-.splide__arrow--prev {
-  left: -0.5rem;
-}
-.splide__arrow--next {
-  right: -0.5rem;
-}
-.splide__arrow svg{
-  fill: #BE8C07;
-  width: 2rem;
-}
-.splide__arrow svg:hover{
-  fill: #ccac53 !important;
-}
-.splide__pagination__page.is-active{
-  background-color: #BE8C07;
-}
-.splide__pagination__page:hover{
-  background-color: #ccac53 !important;
-}
-
-@media screen and (min-width: 768px) {
-  .splide{
-    padding: 3rem;
-  }
-  .splide__arrow {
-    display: flex;
-  }
-  .splide__pagination{
-    display: none;
-  }
-} */
-</style>
