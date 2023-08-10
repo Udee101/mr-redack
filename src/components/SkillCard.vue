4dd54d1f229e7cde2cce7d5760b6845b025579dc
Udeorah Frank <franklynudeorah@gmail.com>
2023-08-10 22:24:00 +0100

    chore:refactor skillCard component

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,16 +1,29 @@
 <script setup>
-defineProps({
-  skill: {
-    type: Object 
-  }
-})
+	const props = defineProps({
+		skill: Object,
+	});
 </script>
 
 <template>
-  <div class="space-y-4 text-center grid place-items-center p-2">
-    <div class=" w-16 h-16 rounded-full bg-white border-[6px] border-site-gold flex place-items-center md:w-32 md:h-32 lg:w-48 lg:h-48">
-      <p class="w-full font-light text-site-gold md:text-3xl lg:text-5xl">{{ skill.proficiency }}</p>
-    </div>
-    <p class="text-white max-w-[220px] text-sm uppercase md:text-base lg:text-lg">{{ skill.title }}</p>
-  </div>
+	<div class="space-y-4 text-center grid place-items-center p-2">
+		<div
+			class="relative w-20 h-20 rounded-full bg-white border-[3px] border-site-gold flex place-items-center md:w-32 md:h-32 lg:w-44 lg:h-44"
+		>
+			<img
+				v-for="(tool, index) in skill.tools"
+				:key="index"
+				:src="tool"
+        alt="images of design/editing software"
+				class="w-[42%] absolute"
+				:class="[
+					index % 2 === 0 ? 'right-[14%] top-[10%]' : 'left-[14%] bottom-[19%]',
+				]"
+			/>
+		</div>
+		<p
+			class="text-white max-w-[220px] text-sm uppercase md:text-base lg:text-lg"
+		>
+			{{ skill.title }}
+		</p>
+	</div>
 </template>
