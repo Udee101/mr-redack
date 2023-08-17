8ccc3a9208c92d5547c6fe81a91881c7620b6455
Udeorah Frank <franklynudeorah@gmail.com>
2023-08-17 09:06:53 +0100

    refactor projects page code

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,39 +1,60 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import {projectData} from "../data/portfolio.js"
-import '@splidejs/vue-splide/css';
-import '@splidejs/vue-splide/css/sea-green';
-import ProjectDetails from '../components/ProjectDetails.vue';
+	import { computed, onMounted, ref } from "vue";
+	import { projectData } from "../data/portfolio.js";
+	import "@splidejs/vue-splide/css";
+	import "@splidejs/vue-splide/css/sea-green";
+	import ProjectDetails from "../components/ProjectDetails.vue";
 
-const props = defineProps({
-  id: {
-    type: String,
-    required: true
-  }
-})
+	const props = defineProps({
+		id: {
+			type: String,
+			required: true,
+		},
+	});
 
-const isLoading = ref(false)
-onMounted(() => {
-  window.scrollTo(0, 0)
-  isLoading.value = true
-  setTimeout(() => {
-    isLoading.value = false
-  }, 2000)
-})
+  const project = computed(() => {
+    return projectData[parseInt(props.id) - 1]
+  })
+
+	const isLoading = ref(false);
+	onMounted(() => {
+		window.scrollTo(0, 0);
+		isLoading.value = true;
+		setTimeout(() => {
+			isLoading.value = false;
+		}, 2000);
+	});
 </script>
-  
+
 <template>
-  <section class="p-4 bg-site-gray">
-    <div v-if="isLoading" class="text-white font-bold h-screen w-full flex items-center mx-auto"><p>LOADING.....</p></div>
-    <div v-else class=" relative max-w-site-w mx-auto min-h-screen py-14">
-      <p class=" hidden absolute right-0 mx-2 py-10 text-site-gold text-4xl font-titleFont md:block">{{ projectData[id-1].tag }}</p>
-      <p class="text-center font-titleFont text-3xl text-site-gold pt-12  md:hidden">{{ projectData[id-1].tag }}</p>
-      <div v-for="(data, index) in projectData[id-1].titles" :key="index" class="min-h-[70vh] flex flex-col">
+  <div
+    v-if="isLoading"
+    class="text-white font-bold min-h-screen w-full grid place-content-center mx-auto bg-site-gray"
+  >
+    <div>LOADING.....</div>
+  </div>
+  <section v-else class="p-4 bg-site-gray">
+    <div class="relative max-w-site-w mx-auto min-h-screen py-14">
+      <p
+        class="hidden absolute right-0 mx-2 py-10 text-site-gold text-4xl font-titleFont md:block"
+      >
+        {{ project.tag }}
+      </p>
+      <p
+        class="text-center font-titleFont text-3xl text-site-gold pt-12 md:hidden"
+      >
+        {{ project.tag }}
+      </p>
+      <div
+        v-for="(titleData, index) in project.titles"
+        :key="index"
+        class="min-h-[70vh] flex flex-col"
+      >
         <div class="grid first:mt-6 md:first:mt-40 md:grid-cols-2 md:gap-x-6">
           <ProjectDetails
-            :category-data="projectData[id-1]"
+            :category-data="project"
             :index="index"
-            :title="data"
+            :title="titleData"
           ></ProjectDetails>
         </div>
       </div>
