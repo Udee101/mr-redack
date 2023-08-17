c644caf0dd9e1ad82dde0f2b1cc7f4f28f79534b
Udeorah Frank <franklynudeorah@gmail.com>
2023-08-17 10:54:39 +0100

    chore:create file for text and image data used in the about page

---
+++ src/data/about.js
@@ -0,0 +1,13 @@
+export const PARAGRAPH_1 = "Isaac Akpoborie is a highly motivated designer and brand consultant driven by a passion for perfection. He possesses exceptional mental strength and resilience, which he applies to tackle briefs and provide innovative design solutions. In addition to his love for graphic design, Isaac is also an avid photographer and video editor.";
+
+export const PARAGRAPH_2 = "Isaac has a wealth of experience in creating and deploying design processes for SMEs across the globe. He began his professional career as a freelance Brand Identity Designer in 2018 and later worked as a Junior Designer at Shefa-Ink Marketing Agency in Ajao Estate in 2020.";
+
+export const PARAGRAPH_3 = "At Shefa-Ink, Isaac honed his design skills while working on various projects for renowned brands such as Makari De Suisse, Adopt, Dr. Teals, Perfect Trust, Somma, CeraVe, La Roche-Posay, and L'Oréal. His scope of service included Social Media Designs, OOH Advertising Designs, Activation Podium Designs & Execution, Editorial Designs, Photography Direction, UI/UX, and Video Editing. In early 2023, Isaac was promoted to the position of Art Director at Shefa-Ink Marketing, where he supervised all of these brands' designs.";
+
+export const PARAGRAPH_4 = "In addition to his professional work, Isaac is also the Founder and Product Design Head at Redack. He has successfully completed over 5,000 marketing materials, brand guidelines, and illustrative projects for various brands across different industries, including cosmetics and retail, tech, pharmaceutical, agricultural, legal, and advertising. He has also worked with manufacturing industries to execute activation materials and podiums.";
+
+export const PARAGRAPH_5 = "Isaac's passion for design and his commitment to excellence make him a valuable asset to any brand seeking to enhance its visual identity and marketing materials. His extensive experience and expertise in design make him a reliable partner for businesses of all sizes and industries.";
+
+export const IMAGEURL_1 = 'https://res.cloudinary.com/dwclpat7w/image/upload/v1692246294/profile-img1_vke9bw.svg';
+
+export const IMAGEURL_2 = 'https://res.cloudinary.com/dwclpat7w/image/upload/v1692246295/profile-img3_acd1ft.svg';
